546c4bc6369f26f5c26bdf154fe3b7283668e830
Mark Smith <markacsmith@gmail.com>
2016-06-25 17:03:02 -0600

    Redo problem set 0 to refresh my memory

---
+++ ProbSet0/pset0-ra.py
@@ -0,0 +1,7 @@
+# Problem set 0 of MIT 6.00 Introduction to computer programming
+# Request input first name and last name store them and print to the screen
+
+
+firstName=input('Enter your First Name ')
+lastname=input('Enter your Last Name ')
+print(firstName, lastname)
