16406fb96061752fe6d70105362b6622350f6a61
MarkSmithlxviii <markacsmith@gmail.com>
2016-08-25 01:07:56 -0600

    changed variable name in ps1

--- ProbSet1/pset1CreditCard.py
+++ ProbSet1/pset1CreditCard.py
@@ -10,21 +10,22 @@
 loan_amount=input("Enter your Credit Card balance in dollars and cents - ")
 minPayRate=input("Enter the Minimum Payment rate - ")
 intRate=input('Enter the interest rate charged by you card provider - ')
-balOut=loan_amount      # This will be the outstandig balance at the end of the
-                        # Calculation Period
-monIntChgs = 0          # The dollar value of the monthly interest
-minMonPay = 0           # The dolar value of the minimum montly payment
-princePd=0                # The dollar value of principle paid monthly
+balOudstndg=loan_amount     # This will be the outstandig balance at the end of the
+                            # Calculation Period
+monIntChgs = 0              # The dollar value of the monthly interest
+minMonPay = 0               # The dolar value of the minimum montly payment
+princePd=0                  # The dollar value of principle paid monthly
 months = range(1,13)
 
 for month in months:
-    monIntChgs = intRate/12* balOut
-    minMonPay = balOut * minPayRate
+    monIntChgs = intRate / 12 * balOudstndg
+    minMonPay = balOudstndg * minPayRate
     if monIntChgs > minMonPay:
         print('Interest charges exceed the minimum payment, ')
         print('the debt cannot be repaid')
-    balOut = balOut-minMonPay+monIntChgs
+    balOudstndg = balOudstndg-minMonPay+monIntChgs
     princePd = minMonPay - monIntChgs
+
     
     
 
