77683d2efaa5e3d76ab1ecce19bab064200a4a2c
MarkSmithlxviii <markacsmith@gmail.com>
2016-08-27 22:42:02 -0700

    Finished the minimum payment calculator problem 1 of Problem Set 1.

--- ProbSet1/pset1CreditCard.py
+++ ProbSet1/pset1CreditCard.py
@@ -22,27 +22,37 @@ monIntChgs = 0          # The dollar value of the monthly interest
 minMonPay = 0           # The dolar value of the minimum monthly payment
 princePd=0              # The dollar value of principle paid monthly
 months = range(1,13)
+totalPaid = 0
+intTotalPaid = 0
+
+# enhancements add a dict of months, and query the OS for today's date
+# have the text for next payment and then the named months for a year 
+# after that.
 
 for month in months:
     monIntChgs = intRate / 12 * balOutStdg
     minMonPay = balOutStdg * minPayRate
     if monIntChgs > minMonPay:
         print('Interest charges exceed the minimum payment, ')
-        print('the debt cannot be repaid')
+        print('the debt cannot be repaid if only the minimum payment is made.')
     princePd = minMonPay - monIntChgs
     balOutStdg = balOutStdg - princePd
+    totalPaid = totalPaid + minMonPay
+    intTotalPaid = intTotalPaid + monIntChgs
     pmon = str(month)                  # Printable MONth
     pint = str(round(monIntChgs, 2))   # Printable INTerest
     ppay = str(round(minMonPay, 2))    # Printable monthly PAYment
     pbal = str(round(balOutStdg, 2))   # Printable outstanding BALance
+    pprin= str(round(princePd, 2))     # Printable PRINciple paid
+    print('')
+    print('Month: ' + pmon)
+    print('')
     print('The interest charges for month ' + pmon + ' are $'+ pint)
     print('The minimum payment for month ' + pmon + ' is $' + ppay)
+    print('The principle paid for month ' + pmon + ' is $' + pprin)
     print('The outstanding balance at the end of month ' + pmon + ' is $' + pbal)
-    
-    
-    
-
-    
-    
-
-
+ptot = str(round(totalPaid,2))
+print('')
+print('')
+print('The total amount paid over 12 months is:            $' + ptot)
+print('The outstanding balance at the end of 12 months is: $' + pbal)
