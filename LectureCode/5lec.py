e352333e306fd8ce09228cafa0af9434de0fb3bf
MarkSmithlxviii <markacsmith@gmail.com>
2016-10-30 16:13:15 -0600

    lecture 5 at 27 minutes unfortunately previous commit work was completed after this work so I get to try out diff for the first time. yay

--- LectureCode/5lec.py
+++ LectureCode/5lec.py
@@ -39,22 +39,22 @@
 L1 = [2]
 L2 = [L1, L1]
 print ('L2 =', L2)
-##L1[0] = 3
-##print 'L2 =', L2
-##L2[0] = 'a'
-##print 'L2 =', L2
-##
+L1[0] = 3
+print ('L2 =', L2)
+L2[0] = ('a')
+print ('L2 =', L2)
+
 ##L1 = [2]
-##print 'L2 =', L2
-####L2 = L1
-####L2[0] = 'a'
-####print 'L1 =', L1
-####print 'L2 =', L2
+##print ('L2 =', L2)
+##L2 = L1
+##L2[0] = 'a'
+##print ('L1 =', L1)
+##print ('L2 =', L2)
 ##
 ##L1 = [2]
 ##L2 = L1[:]
 ##L2[0] = 'a'
-##print 'L1 =', L1
+##print ('L1 =', L1)
 
 ##def copyList(LSource, LDest):
 ##    for e in LSource:
