cd349397d1d9f86017572c43ffc96d2e5fff2fae
MarkSmithlxviii <markacsmith@gmail.com>
2017-01-01 07:13:22 -0800

    Lecture watched handout code updated to Python 3

--- mit600/handouts/6lec.py
+++ mit600/handouts/6lec.py
@@ -30,30 +30,30 @@
 ##print (translate('Eric drinks wine'))
 ##print (translate('Everyone likes 6.00'))
 ##
-##def toChars(s):
-##    import string
-##    s = string.lower(s)
-##    ans = ''
-##    for c in s:
-##        if c in string.lowercase:
-##            ans = ans + c
-##    return ans
-##
-##def isPal(s):
-##    if len(s) <= 1:
-##        return True
-##    else:
-##        return s[0] == s[-1] and isPal(s[1:-1])
-##
-##def isPalindrome(s):
-##    """Returns True if s is a palindrome and False otherwise"""
-##    return isPal(toChars(s))
-##
-####print isPalindrome('Guttag')
-####print isPalindrome('Guttug')
-####print isPalindrome('Able was I ere I saw Elba')
-####print isPalindrome('Are we not drawn onward, we few, drawn onward to new era?')
-##
+def toChars(s):
+    import string
+    s = s.lower()
+    ans = ''
+    for c in s:
+        if c in string.ascii_lowercase:
+            ans = ans + c
+    return ans
+
+def isPal(s):
+    if len(s) <= 1:
+        return True
+    else:
+        return s[0] == s[-1] and isPal(s[1:-1])
+
+def isPalindrome(s):
+    """Returns True if s is a palindrome and False otherwise"""
+    return isPal(toChars(s))
+
+print( isPalindrome('Guttag'))
+print( isPalindrome('Guttug'))
+print( isPalindrome('Able was I ere I saw Elba'))
+print (isPalindrome('Are we not drawn onward, we few, drawn onward to new era?'))
+
 ##def fib(x):
 ##    """assumes x an int >= 0
 ##        Returns Fibonacci of x"""
